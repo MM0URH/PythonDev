5b5b57388817ae9314ba99e61eaa91a2db6c1cc1
Gordie <stirlig@gmail.com>
2021-02-14 13:25:20 +0000

    Simple Calculator to convert from celsius to fahrenheit and vice versa

---
+++ Calculators/tempCalcC2F.py
@@ -0,0 +1,4 @@
+inpt = float(input('What is the Temperature in C: '))
+workout = float(((inpt*9)/5)+32)
+outpt = round(workout,1)
+print("Temp in C is ", outpt , "Degrees")
---
+++ Calculators/tempCalcF2C.py
@@ -0,0 +1,5 @@
+inpt = float(input('What is the Temperature in F: '))
+workout = float(((inpt-32)*5)/9)
+outpt = round(workout,1)
+print("Temp in C is ", outpt , "Degrees")
+
